e5c944a4c99da46feca39853dab0a47f7e444252
Thales <thales.marcelino@gmail.com>
2024-03-07 14:43:12 -0300

    feat(user.details): Creating the Details Component Layout

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from "@angular/core";
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   imports : [
     MatButtonModule,
+    MatListModule,
+    MatDividerModule,
+
   ],
-  exports : [],
+  exports : [
+    MatListModule,
+    MatDividerModule
+  ],
 })
 export class AngularMaterialModule {
 
