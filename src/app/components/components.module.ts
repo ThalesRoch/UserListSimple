93cb10537bbbb095058208624f99320a0c2f08c5
Thales <thales.marcelino@gmail.com>
2024-03-07 17:58:36 -0300

    feat(phone.pipe): Phone Pipe Creation

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,6 +6,7 @@ import { AngularMaterialModule } from '../angular-material/angular-material.modu
 import { FilterComponent } from './filter/filter.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
   declarations : [
@@ -17,6 +18,7 @@ import { UsersListComponent } from './users-list/users-list.component';
     AngularMaterialModule,
     FormsModule,
     CommonModule,
+    PipesModule,
   ],
   exports : [
     UserDetailsComponent,
