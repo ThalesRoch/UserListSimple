1186e9abef2d16de9ce4a5806b9fdda6877baed3
Thales <thales.marcelino@gmail.com>
2024-03-11 11:57:19 -0300

    feat: Creating the Click Event on the Filter Button

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IFilterOptions } from '../../interfaces/filter-options-interface';
 
 @Component({
@@ -17,8 +17,10 @@ export class FilterComponent {
     { description: 'Ativo', value: true },
     { description: 'Inaivo', value: false },
   ];
+
+  @Output('onFilter') onFilterEmitt = new EventEmitter<IFilterOptions>();
   onFilter() {
-    console.log(this.filterOptions);
+    this.onFilterEmitt.emit(this.filterOptions);
   };
 
 }
